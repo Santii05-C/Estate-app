15b31c6565bfacdcec317291afe934862e7cebf4
Santii05-C <santi.c.pgm@gmail.com>
2024-06-21 13:02:59 -0300

    last commit

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -1,15 +1,21 @@
 import jwt from "jsonwebtoken";
 
 export const shouldBeLoggedIn = async (req, res) => {
+  console.log(req.userId);
+
+  res.status(200).json({ messge: "You are Authenticated!" });
+};
+
+export const shoulBeAdmin = async (req, res) => {
   const token = req.cookies.token;
 
   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) return;
-    res.status(403).json({ message: "Token is in valid !" });
+    if (err) return res.status(403).json({ message: "Token is in valid !" });
+    if (!payload.isAdmin) {
+      res.status(403).json({ message: "Not authorized!" });
+    }
   });
   res.status(200).json({ messge: "You are Authenticated!" });
 };
-
-export const shoulBeAdmin = async (req, res) => {};
