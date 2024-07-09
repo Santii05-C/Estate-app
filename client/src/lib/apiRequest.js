2874915ae5248503c3b3a302be6373ccd8d89fb6
Santii05-C <santi.c.pgm@gmail.com>
2024-07-09 11:44:54 -0300

    last commit

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
+  // baseURL: "http://localhost:8800/api",
   baseURL: "https://backend-estate-1.onrender.com/api",
   withCredentials: true,
 });
